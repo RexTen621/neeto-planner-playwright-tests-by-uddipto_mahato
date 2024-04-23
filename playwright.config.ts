ed484ba4b4317e4b486b7a22b432f891e4a6f285
uddipto <uddiptamahato621@gmail.com>
2024-04-23 20:28:29 +0530

    Removed comments from config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,22 +4,19 @@ export const STORAGE_STATE = "./auth/session.json";
 
 export default defineConfig({
   testDir: './e2e',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  fullyParallel: true,
+
+  forbidOnly: !!process.env.CI,
+ 
+  retries: process.env.CI ? 2 : 0,
+ 
+  workers: process.env.CI ? 1 : undefined,
+ 
+  reporter: 'html',
+
+  use: {
+   
     trace: 'on-first-retry',
     testIdAttribute: "data-cy",
     baseURL:"https://app.neetoauth.net/login"
