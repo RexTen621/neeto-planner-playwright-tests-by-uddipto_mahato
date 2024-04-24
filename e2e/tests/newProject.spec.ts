0517def5919edb80a502e7beb9ff9173e0ff65a3
uddipto <uddiptamahato621@gmail.com>
2024-04-24 16:53:18 +0530

    creation of new branch for the test

--- e2e/tests/newProject.spec.ts
+++ e2e/tests/newProject.spec.ts
@@ -2,22 +2,22 @@ import test from "../Fixtures/testFixtures"
 import { faker } from "@faker-js/faker"
 
 
-test.describe("Should login and create a new project",()=>{
+test.describe("Should login and create a new project", () => {
 
-    let newProjectName:string
-    let newProjectDescription:string
+    let newProjectName: string
+    let newProjectDescription: string
 
-    test.beforeEach(()=>{
+    test.beforeEach(() => {
 
-        newProjectName=faker.word.words({count:3})
-        newProjectDescription=faker.word.words({count:5})
+        newProjectName = faker.word.words({ count: 3 })
+        newProjectDescription = faker.word.words({ count: 5 })
 
     })
-    test("create a new project",async({page,projectPage})=>{
+    test("create a new project", async ({ page, projectPage }) => {
 
-        await test.step("Step 1:Should login and verify",()=> page.goto('/'))
+        await test.step("Step 1:Should login and verify", () => page.goto('/'))
 
-        await test.step("Step 2: Should create a new project and verify",()=>projectPage.addNewProject({projectName:newProjectName,projectDescription:newProjectDescription}))
+        await test.step("Step 2: Should create a new project and verify", () => projectPage.addNewProject({ projectName: newProjectName, projectDescription: newProjectDescription }))
 
     })
 })
