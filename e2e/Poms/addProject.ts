dfdf07c30700488c04dd1712efe8869a510912bf
RexTen621 <141172311+RexTen621@users.noreply.github.com>
2024-04-25 04:23:36 +0530

    Updated addProject.ts

--- e2e/Poms/addProject.ts
+++ e2e/Poms/addProject.ts
@@ -18,26 +18,5 @@ export default class ProjectPage {
         await expect(this.page.getByTestId(ADD_PROJECT_SELECTORS.mainHeaderTestId)).toHaveText(projectName)
     }
 
-    assignNewTaskInProject=async({taskName,username,taskDescription,taskComment}:{taskName:string,username:string,taskDescription:string,taskComment:string})=>{
-
-        await this.page.locator("button.neeto-ui-btn.ml-8").click()
-        await this.page.getByTestId("nui-input-field").fill(taskName)
-        await this.page.locator("button[data-testid='neeto-molecules-autosave-input-save']").click()
-        const getTaskRow=this.page.locator("table[data-rfd-droppable-id='sections']").getByRole("row",{name: new RegExp(taskName,"i")})
-      
-        await getTaskRow.locator("(//button[contains(@class,'neeto-ui-btn neeto-ui-border-gray-500')])[3]").click()
-        await this.page.locator(".neeto-ui-dropdown__popup-menu-item-btn").getByText(username).click()
-        await getTaskRow.locator("(//div[contains(@class,'hover:neeto-ui-text-primary-800 flex')])[3]").click()
-        await this.page.waitForTimeout(5000)
-        await expect(this.page.locator("(//button[contains(@class,'neeto-ui-btn neeto-ui-btn--style-secondary')])[2]")).toHaveText("Add checklist")
-        await this.page.locator("(//div[@class='flex items-center'])[1]").click()
-        await this.page.locator("(//div[@data-cy='neeto-editor-content'])[1]").fill(taskDescription)
-
-        await this.page.locator("button.neeto-ui-btn.neeto-ui-btn--style-primary.neeto-ui-btn--size-medium").click()
-        await this.page.locator("(//div[@data-cy='neeto-editor-content'])[2]").fill(taskComment)
-        await this.page.locator("button[data-cy='chat-post-button']").click()
-        await this.page.getByTestId("pane-close-button").click()
-
-
-    }
+   
 }
